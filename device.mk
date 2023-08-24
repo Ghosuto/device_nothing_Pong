afc86bdfd80afce897623cccad71175209684711
TogoFire <togofire@mailfence.com>
2023-08-24 12:43:55 +0000

    Pong: Build ViPER4AndroidFX
    Viper's Audio is software for various platforms, such as Android and PC, Cat.
    It is used to improve the sound in general, with the help of various sound effects,
    i.e. it is actually a system equalizer.
    
    Reference:
    https://github.com/xiaomi-begonia-dev/android_packages_apps_ViPER4AndroidFX
    
    Signed-off-by: athizz2005 <atharva15.bgmi@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- device.mk
+++ device.mk
@@ -520,6 +520,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# Build ViPER4AndroidFX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
