890d503fb1df40dc11e83a0c761e52110654dbdc
Ghosuto <clash.raja10@gmail.com>
2024-03-19 01:40:06 +0530

    Pong: add vendorsetup.sh - Kernel, display hal, vendor, sepolicy

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+rm -rf hardware/qcom-caf/sm8450/display
+git clone https://github.com/Nothing-phone-2-Development/android_hardware_qcom_display.git hardware/qcom-caf/sm8450/display
+git clone https://gitlab.com/GhosutoX/android_vendor_nothing_Pong.git vendor/nothing/Pong
+git clone https://github.com/HELLBOY017/kernel_nothing_sm8475.git -b inline kernel/nothing/sm8475
+rm -rf device/qcom/sepolicy_vndr/sm8450 
+git clone https://github.com/ProjectGhostOS/device_qcom_sepolicy_vndr.git device/qcom/sepolicy_vndr/sm8450
