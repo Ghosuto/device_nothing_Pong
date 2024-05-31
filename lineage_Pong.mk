7109bc1096af7b2f95f72c0c404e6cd4c1805de0
Ghosuto <clash.raja10@gmail.com>
2024-05-31 15:58:33 +0000

    Pong: Build BCR

--- lineage_Pong.mk
+++ lineage_Pong.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/nothing/Pong/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# BCR
+$(call inherit-product, vendor/bcr/bcr.mk)
+
 PRODUCT_NAME := lineage_Pong
 PRODUCT_DEVICE := Pong
 PRODUCT_MANUFACTURER := nothing
