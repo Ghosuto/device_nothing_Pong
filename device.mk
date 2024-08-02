9e9ed460b7ec8f4faa8de6d5f2359905b9f8c1ee
Ghosuto <clash.raja10@gmail.com>
2024-08-02 14:47:21 +0000

    new

--- device.mk
+++ device.mk
@@ -345,6 +345,7 @@ PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libcodec2_hidl@1.0.vendor \
+    libcodec2_soft_common.vendor \
     libcodec2_vndk.vendor \
     libpalclient
 
